747709f04d107562abb9eab09242b8bd1be2886a
Risqi <khoirulrizki029@gmail.com>
2023-12-23 22:58:24 +0700

    fix

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign in - Voler Admin Dashboard</title>
+    <title>Sign in -Admin Dashboard</title>
     <link rel="stylesheet" href="<?= base_url("assets/css/bootstrap.css"); ?>">
     
     <link rel="shortcut icon" href="<?= base_url("assets/images/favicon.svg"); ?>" type="image/x-icon">
